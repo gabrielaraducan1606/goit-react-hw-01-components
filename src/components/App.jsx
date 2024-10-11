fec57d129641d21606878214d4a46967bea67199
gabrielaraducan1606 <165211880+gabrielaraducan1606@users.noreply.github.com>
2024-10-11 22:31:27 +0300

    commit1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import Profile from './profile/profile';
+import Statistics from './statistics/statistics';
+import FriendList from './friendList/friendList';
+import TransactionHistory from './transactionHistory/transactionHistory';
+
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/statistics">Statistics</Link></li>
+          <li><Link to="/friend-list">Friend List</Link></li>
+          <li><Link to="/transactions">Transactions</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+  <Route path="/profile" element={<Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />} />
+  
+  <Route path="/statistics" element={<Statistics title="Upload stats" stats={data} />} />
+  
+  <Route path="/friend-list" element={<FriendList friends={friends} />} />
+  
+  <Route path="/transactions" element={<TransactionHistory items={transactions} />} />
+</Routes>
+    </Router>
   );
 };
+
+export default App;
